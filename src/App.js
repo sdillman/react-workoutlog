ac6e74a4cc5dbc4ca6ca6009fad4d91c05166b43
Shannon D <sdillman@users.noreply.github.com>
2021-07-01 18:41:31 -0400

    10.2 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Component, useState } from 'react';
 // import './App.css';
+import Sitebar from './home/Navbar';
+import Auth from './auth/Auth';
+
 
 function App() {
+  const [sessionToken, setSessionToken] = useState(''); //1
 
-  useEffect (() => {
+  useEffect (() => {  //2
     document.title = "Workout Log Client";
+
+    if (localStorage.getItem('token')) {
+      setSessionToken(localStorage.getItem('token'));
+    }
   }, []);
+
+  const updateToken = (newToken) => { //3
+    localStorage.setItem('token', newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  }
+  //render method is down here
 
   return (
     <div>
-      
+      <Sitebar />
+      <Auth />
     </div>
   );
 }
