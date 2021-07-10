ef1e783da9af720097dd39c618ade7c85b62a855
Shannon D <sdillman@users.noreply.github.com>
2021-07-10 14:12:02 -0400

    Added 'Bearer ' to my jwt call, which fixed server issues. 10.6 done

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -9,7 +9,7 @@ const handleSubmit = (event) => {
     event.preventDefault();
     fetch("http://localhost:3000/user/register", { 
         method: 'POST',
-        body: JSON.stringify({user:{username: username, password: password}}),
+        body: JSON.stringify({user:{username: username, passwordhash: password}}),
         headers: new Headers({
             'Content-Type': 'application/json'
         })
