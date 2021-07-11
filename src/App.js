2e99f85e59d7a8c5b9ed9a66290b62048a41e7de
Shannon D <sdillman@users.noreply.github.com>
2021-07-11 01:01:56 -0400

    OMG I ACTUALLY FIXED MY BUGS. VICTORY IS MINE. It works in full

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Component, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import './App.css';
 import Sitebar from './home/Navbar';
 import Auth from './auth/Auth';
--- src/workouts/WorkoutIndex.js
+++ src/workouts/WorkoutIndex.js
@@ -1,15 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import { Container, Row, Col} from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import WorkoutCreate from './WorkoutCreate';
+import WorkoutTable from './WorkoutTable';
+import WorkoutEdit from './WorkoutEdit';
 
 const WorkoutIndex = (props) => {
     const [workouts, setWorkouts] = useState([]);
+    const [updateActive, setUpdateActive] = useState(false);
+    const [workoutToUpdate, setWorkoutToUpdate] = useState({});
 
     const fetchWorkouts = () => {
         fetch('http://localhost:3000/log', {
             method: 'GET',
             headers: new Headers ({
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + props.token
+                'Authorization': 'Bearer '+props.token
             })
         }).then( (res) => res.json())
         .then( (logData) => {
@@ -18,19 +23,33 @@ const WorkoutIndex = (props) => {
         })
     }
 
+    const editUpdateWorkout = (workout) => {
+        setWorkoutToUpdate(workout);
+        console.log(`WORKOUT: ${workout}  WORKOUT.ID: ${workout.id}`);
+    }
+    const updateOn = () => {
+        setUpdateActive(true);
+    }
+    const updateOff = () => {
+        setUpdateActive(false);
+    }
+
     useEffect(() => {
         fetchWorkouts();
     }, [])
+
+//doesn't seem to matter if  'Bearer '+props.token  is used in the tokens below or not. Attempts to update result in a 404 at WorkoutEdit.js line 11
 
     return (
         <Container>
             <Row>
                 <Col md="3">
-                    { /* The Create component will go here */}
+                    <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
                 </Col>
                 <Col md="9">
-                    <h2>Log a workout to see a table. This will be added in later pages.</h2>
+                    <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token}/>
                 </Col>
+                {updateActive ? <WorkoutEdit workoutToUpdate={workoutToUpdate} updateOff={updateOff} token={'Bearer '+props.token} fetchWorkouts={fetchWorkouts}/> : <> </>}
             </Row>
         </Container>
     )
